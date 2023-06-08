2d11915804b73dc3c00c348b3633cfaab034d1bb
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-06-08 08:28:29 +0000

    e

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
 	<svg width="100%" height="100%" viewBox="0 0 5216 927" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<img src="/banner.png" alt="logo" />
+		<img src="https://cosmicord.now.sh/banner.png" alt="logo" />
 	</svg>
 </template>
