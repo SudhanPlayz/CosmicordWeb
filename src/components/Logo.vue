227396c8fa02ffb219b932195a41b1bde20a82f3
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-06-08 08:18:10 +0000

    fix

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
 	<svg width="100%" height="100%" viewBox="0 0 5216 927" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<img src="/logo" alt="logo" />
+		<img src="/logo.png" alt="logo" />
 	</svg>
 </template>
