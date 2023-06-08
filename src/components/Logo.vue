c8a8430ebb940ca49a495c7355ed888c4d34fbc3
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-06-08 08:42:51 +0000

    a

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,3 @@
 <template>
-	<img width="100%" height="100%" src="https://media.discordapp.net/attachments/1113373125086814320/1116284868897296384/CosmicordBanner.png" alt="logo" />
+	<img src="https://media.discordapp.net/attachments/1113373125086814320/1116284868897296384/CosmicordBanner.png" alt="logo" />
 </template>
