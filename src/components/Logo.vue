419d124999941483b7b4766372a015bddaab5c14
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-06-08 08:40:40 +0000

    removed svg

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,3 @@
 <template>
-	<svg width="100%" height="100%" viewBox="0 0 5216 927" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<img src="https://media.discordapp.net/attachments/1113373125086814320/1116284868897296384/CosmicordBanner.png" alt="logo" />
-	</svg>
+	<img width="100%" height="100%" src="https://media.discordapp.net/attachments/1113373125086814320/1116284868897296384/CosmicordBanner.png" alt="logo" />
 </template>
